2d63c5270e0cd5fafa554a470839e9b877b236d5
essale <itay.ess10@gmail.com>
2021-01-10 13:05:08 +0200

    fix install script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t reverse-proxy .
-docker run -p 8081:8081 -e URL=<url here> -e PORT=<port here> reverse-proxy
+docker run -p 8081:8081 -e URL=<url here> -e PORT=<port here> itayess/reverse-proxy
