c1fece20067724f4d0bdb3582daa2d8c80550348
essale <itay.ess10@gmail.com>
2021-01-10 18:53:36 +0200

    Support https

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 docker build -t reverse-proxy .
-docker run -p 8081:8081 -e URL=<url here> -e PORT=<port here> itayess/reverse-proxy
+docker run \
+  --name reverse-proxy \
+  -d \
+  --restart always  \
+  -e URL=<url here> \
+  -v <path on server where keystore located>:/keystore \
+  -e KEY_STORE_PATH=<this is the keystore path> \
+  -e KEY_STORE_PASS=<this is the keystore pass> \
+  -e KEY_STORE_TYPE=<default: JKS> \
+  -e PORT=8081 \
+  -p 8081:8081 \
+  itayess/reverse-proxy
